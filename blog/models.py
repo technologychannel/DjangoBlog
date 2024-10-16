e949109043717572b388bed144324f9710747ba4
Thulo Technology <thulotechnology@gmail.com>
2024-10-16 18:52:05 +0545

    Image Upload

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    image = models.ImageField(upload_to='blog/images/', default='blog/images/default.jpg')
+    thumbnail = models.ImageField(upload_to='blog/images/', default='blog/images/thumb.jpg')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from . import views
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('',views.index,name='homepage'),
     path('post/<int:id>', views.single_page, name='single')
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
